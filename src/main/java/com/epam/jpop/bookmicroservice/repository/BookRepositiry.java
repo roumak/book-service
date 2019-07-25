f2e71b8be623b41090027bfb7df0efdd10340e68
Roumak Chakraborty <Roumak.Chakraborty@mastercard.com>
2019-07-25 15:42:44 +0530

    reset

--- src/main/java/com/epam/jpop/bookmicroservice/repository/BookRepositiry.java
+++
@@ -1,7 +0,0 @@
-package com.epam.jpop.bookmicroservice.repository;
-
-import com.epam.jpop.bookmicroservice.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepositiry extends JpaRepository<Book,Long> {
-}
