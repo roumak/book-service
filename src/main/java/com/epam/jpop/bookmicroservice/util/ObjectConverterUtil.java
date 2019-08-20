1fc890ae900dd0bc4304ec792450ae2ae22fecb8
Roumak Chakraborty <Roumak.Chakraborty@mastercard.com>
2019-08-20 12:32:27 +0530

    adding object mapper

---
+++ src/main/java/com/epam/jpop/bookmicroservice/util/ObjectConverterUtil.java
@@ -0,0 +1,24 @@
+package com.epam.jpop.bookmicroservice.util;
+
+import org.modelmapper.ModelMapper;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ObjectConverterUtil{
+
+    private static ModelMapper modelMapper= new ModelMapper();
+
+    public static <S,D> D convert(final S source, Class<D> destinationClazz){
+        return modelMapper.map(source,destinationClazz);
+    }
+
+    public static<D,C> List<D> convertAll(final Collection<C> collection, Class<D> destinationClazz){
+        return collection.stream()
+                .map(eachElementOfCollection -> convert(eachElementOfCollection, destinationClazz))
+                .collect(Collectors.toList());
+    }
+
+
+}
