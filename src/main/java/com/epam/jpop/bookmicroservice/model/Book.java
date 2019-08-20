efabdac21e5b603ff3f1596112120c01c30c0abf
Roumak Chakraborty <Roumak.Chakraborty@mastercard.com>
2019-08-20 12:11:37 +0530

    adding lombok changes

--- src/main/java/com/epam/jpop/bookmicroservice/model/Book.java
+++ src/main/java/com/epam/jpop/bookmicroservice/model/Book.java
@@ -1,12 +1,16 @@
 package com.epam.jpop.bookmicroservice.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Data;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "book")
 @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+@Data
+@Builder
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,58 +29,4 @@ public class Book {
     @Column(name = "desciption")
     private String description;
 
-    public Book(String bookName, String authorName, String category, String description) {
-        this.bookName = bookName;
-        this.authorName = authorName;
-        this.category = category;
-        this.description = description;
-    }
-    public Book(Long id,String bookName, String authorName, String category, String description) {
-        this.bookId=id;
-        this.bookName = bookName;
-        this.authorName = authorName;
-        this.category = category;
-        this.description = description;
-    }
-    public Book(){}
-
-    public Long getBookId() {
-        return bookId;
-    }
-
-    public void setBookId(Long bookId) {
-        this.bookId = bookId;
-    }
-
-    public String getBookName() {
-        return bookName;
-    }
-
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
-
-    public String getAuthorName() {
-        return authorName;
-    }
-
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
