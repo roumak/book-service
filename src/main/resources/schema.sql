e14cf235ff5cc998c90be667e83b10ebc00b9a43
this_rc <rc.roumak@gmail.com>
2020-07-06 20:16:59 +0530

    changed book type from ENUM to VARCHAR

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE book
     book_isbn VARCHAR(13) NOT NULL UNIQUE,
     book_title VARCHAR(40),
     author_name VARCHAR(50),
-    book_type ENUM('SOFTCOVER', 'HARDCOVER', 'EBOOK', 'AUDIOBOOK', 'COLLECTIONS'),
+    book_type VARCHAR(20),
     book_mrp DECIMAL(10,2),
     book_cp DECIMAL(10,2) NOT NULL,
     book_discount DECIMAL (4,2),
