14d373aa653d27ae883d84ccc3b8aa0eb2f0bc9e
this_rc <rc.roumak@gmail.com>
2020-08-09 03:32:14 +0530

    changed dto structure

--- src/main/java/com/this_rc/bookmicroservice/infrastructure/controller/ResponseBookDto.java
+++ src/main/java/com/this_rc/bookmicroservice/infrastructure/controller/ResponseBookDto.java
@@ -1,11 +1,7 @@
-package com.this_rc.bookmicroservice.domain;
+package com.this_rc.bookmicroservice.infrastructure.controller;
 
 import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -13,49 +9,52 @@ import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @ApiModel(description = "Book details")
-@NoArgsConstructor
 @AllArgsConstructor
-@Setter
-@Getter
-@Builder
-public class InternalBookDto {
+class ResponseBookDto {
     @NotNull
     @NotEmpty
-    private Long bookId;
+    public Long bookId;
 
     @NotNull
     @NotEmpty
-    private String bookIsbn;
+    public String bookIsbn;
 
     @NotNull
     @NotEmpty
     @Size(max = 40, message = "book name should be between 2 and 40")
-    private String bookTitle;
+    public String bookTitle;
 
     @NotNull
     @NotEmpty
     @Size(min = 2, max = 40, message = "author name should be between 2 and 40")
-    private String authorName;
-
-    @NotNull
-    @NotEmpty(message = "MRP must not be null or empty")
-    private BigDecimal maxRetailPrice;
-
-    @NotNull
-    @NotEmpty(message = "discount must not be null or empty")
-    private BigDecimal discount;
-
-    @NotNull
-    @NotEmpty(message = "cost price must not be null or empty")
-    private BigDecimal costPrice;
-
-    @Size(min = 1, max = 30, message = "category must be between 1 and 30 characters")
-    private String category;
-
-    @Size(max = 150, message = "description must be less than 150 characters")
-    private String description;
+    public String authorName;
 
     @NotNull
     @NotEmpty
-    private int count;
+    @Size(min = 5, max = 20, message = "type should be between 5 and 20")
+    public String type;
+
+    @NotNull
+    @NotEmpty(message = "MRP must not be null or empty")
+    public BigDecimal maxRetailPrice;
+
+    @NotNull
+    @NotEmpty(message = "MRP must not be null or empty")
+    public BigDecimal discount;
+
+    @Size(min = 1, max = 30, message = "category must be between 1 and 30 characters")
+    public String category;
+
+    @Size(max = 150, message = "description must be less than 150 characters")
+    public String description;
+
+    @NotNull
+    @NotEmpty
+    public int count;
+
+    @Override
+    public String toString() {
+        return bookTitle + " " + authorName + " " + category;
+    }
+
 }
