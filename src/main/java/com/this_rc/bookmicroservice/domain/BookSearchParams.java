9bcf650dc23f862a9db599c0b86bef7fa18240d9
this_rc <rc.roumak@gmail.com>
2020-08-15 18:04:49 +0530

    refactoring to hexagonal archtecture

---
+++ src/main/java/com/this_rc/bookmicroservice/domain/BookSearchParams.java
@@ -0,0 +1,7 @@
+package com.this_rc.bookmicroservice.domain;
+
+class BookSearchParams {
+    boolean isEmpty() {
+      throw new  UnsupportedOperationException("need tp implement this");
+    }
+}
